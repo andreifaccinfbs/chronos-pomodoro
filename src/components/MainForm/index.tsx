8fd82deebfc34a06424e2f399d91a04700ab27ff
Andrei Faccin <andrei.faccin.ti@gmail.com>
2025-10-28 17:18:34 -0300

    dia 07

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -9,11 +9,12 @@ import type { TaskModel } from "../../models/TaskModel";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from "../../utils/getNextCycleType";
-import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
+import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
+import { Tips } from "../tips";
 
 // Formulário principal da aplicação
 export function MainForm() {
-  const { state, setState } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
   //const [taskName, setTaskName] = useState("");
   const taskNameInput = useRef<HTMLInputElement>(null);
 
@@ -41,36 +42,13 @@ export function MainForm() {
       type: nextCycleType,
     };
 
-    const secondsRemaining = newTask.duration * 60;
-
-    setState((prevState) => {
-      return {
-        ...prevState,
-        config: { ...prevState.config },
-        activeTask: newTask,
-        currentCycle: nextCycle, // conferir dps
-        secondsRemaining,
-        formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining),
-        tasks: [...prevState.tasks, newTask],
-      };
+    dispatch({
+      type: TaskActionTypes.START_TASK,
+      payload: newTask,
     });
   }
-
   function handleInterruptTask() {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        activeTask: null,
-        secondsRemaining: 0,
-        formattedSecondsRemaining: "00:00",
-        tasks: prevState.tasks.map((task) => {
-          if (prevState.activeTask && prevState.activeTask.id === task.id) {
-            return { ...task, interruptDate: Date.now() };
-          }
-          return task;
-        }),
-      };
-    });
+    dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
   }
 
   return (
@@ -87,7 +65,7 @@ export function MainForm() {
       </div>
       {/* Texto informativo (temporário) */}
       <div className="formRow">
-        <p>Próximo intervalo é de 25 min</p>
+        <Tips />
       </div>
       {/* Componente que mostra os ciclos */}
       <div className="formRow">
