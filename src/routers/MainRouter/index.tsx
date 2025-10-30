684d124b1f527357df63173b289f4c3e032a5e4f
Andrei Faccin <andrei.faccin.ti@gmail.com>
2025-10-30 17:11:33 -0300

    dia 09

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -3,6 +3,8 @@ import { NotFound } from "../../pages/NotFound";
 import { Home } from "../../pages/home";
 import { AboutPomodoro } from "../../pages/AboutPomodoro";
 import { useEffect } from "react";
+import { History } from "../../pages/History";
+import { Settings } from "../../pages/Settings";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -20,6 +22,9 @@ export function MainRouter() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about-pomodoro/" element={<AboutPomodoro />} />
+        <Route path="/settings/" element={<Settings />} />
+        <Route path="/history/" element={<History />} />
+
         <Route path="*" element={<NotFound />} />
       </Routes>
       <ScrollToTop />
